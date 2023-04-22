b4d6019f53a967f551726caf9291c305fb60b8c1
unknown <Z.ELHarmouzi@aui.ma>
2023-04-22 03:25:32 +0000

    added a comment

--- zaki/src/supabaseClient.js
+++ zaki/src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
+// hide these lines after
 const supabaseUrl = 'https://pommuyglcsovkglmrvoc.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvbW11eWdsY3NvdmtnbG1ydm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA0MDI3MzQsImV4cCI6MTk5NTk3ODczNH0.A3y0whp8_rLctvvuueBoT95P0xhCCQh6l4lEIPPvWAY'
 export const supabase = createClient(supabaseUrl, supabaseKey)
